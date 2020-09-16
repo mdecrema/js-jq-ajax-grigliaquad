45d64a10a6612c067e045830c0972d9fa69a1567
marco <marcodecrema@libero.it>
2020-09-16 16:43:09 +0200

    prova indirizzo http

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
 
+  $("#uno").click(function() {
+    $.ajax({
+    url: "http://flynn.boolean.careers/exercises/api/random/int",
+    method: "GET",
+    success: function (data, stato) {
+      console.log(data);
+    },
+    error: function (richiesta, stato, errori) {
+      alert("Errore: "+errori);
+    }
+  })
+  })
+
+var num = Math.floor(Math.random() * 10)+1;
+
   var quad = {
     "quadratino": "qua",
-    "numero": Math.floor(Math.random() * 10)+1,
+    "numero": num,
   }
-
-  //var num = Math.floor(Math.random() * 10)+1;
 
   var source = $("#entry-template").html();
   var template = Handlebars.compile(source);
